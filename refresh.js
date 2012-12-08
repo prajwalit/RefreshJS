8cafda94f24b8dbf0e7d99ca6f22b166524a1d55
Prajwalit Bhopale <prajwalit@helpshift.com>
2012-12-08 14:47:56 +0530

    Fix MIME types.

--- refresh.js
+++ refresh.js
@@ -16,6 +16,23 @@ var connection, currentWatchers = [], settings, domains;
 // Routes
 app.use ("/static/", function (request, response) {
   var url = request.url.split ("?") [0];
+  var type = url.split (".");
+  type = type [type.length - 1];
+
+  switch (type) {
+  case "css":
+    response.setHeader ("Content-Type", "text/css");
+    break;
+  case "js":
+    response.setHeader ("Content-Type", "application/javascript");
+    break;
+  case "png":
+  case "jpg":
+  case "jpeg":
+  case "gif":
+    response.setHeader ("Content-Type", "image/" + type);
+    break;
+  }
   response.end (fs.readFileSync ("resources/static" + url));
 });
 
